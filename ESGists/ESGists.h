1c55144a8ec6a15e2086a783b65eed570c6c9ed4
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-08-03 06:30:36 +0900

    Initial Commit

---
+++ ESGists/ESGists.h
@@ -0,0 +1,19 @@
+//
+//  ESGists.h
+//  ESGists
+//
+//  Created by Tomohiro Kumagai on H27/08/03.
+//  Copyright © 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ESGists.
+FOUNDATION_EXPORT double ESGistsVersionNumber;
+
+//! Project version string for ESGists.
+FOUNDATION_EXPORT const unsigned char ESGistsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ESGists/PublicHeader.h>
+
+
