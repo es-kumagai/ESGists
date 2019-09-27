45259f945220f8c0ceee1953b6c19a206cdf2909
Tomohiro Kumagai <tomohiro@ez-style.jp>
2019-09-28 01:57:00 +0900

    Fix invalid format.

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/es-kumagai/Ocean", .branch("master")),
-        .package(url: "https://github.com/ishkawa/APIKit", "5.0.0"..<),
-        .package(url: "https://github.com/ikesyo/Himotoki", "4.0.0"..<),
+        .package(url: "https://github.com/ishkawa/APIKit", "5.0.0" ..< "6.0.0"),
+        .package(url: "https://github.com/ikesyo/Himotoki", "4.0.0" ..< "5.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
