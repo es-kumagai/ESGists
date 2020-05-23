e8d1d14a1b63d25fea36d3eb1df926396cd16593
Tomohiro Kumagai <tomohiro@ez-style.jp>
2020-05-23 13:59:47 +0900

    Unspecify library type.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ESGists",
-            type: .dynamic,
             targets: ["ESGists"]),
     ],
     dependencies: [
